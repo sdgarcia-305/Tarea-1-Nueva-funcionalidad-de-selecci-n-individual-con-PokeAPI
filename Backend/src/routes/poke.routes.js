89fe9bcadb228a3fdd24de2b2add1ff469758851
sdgarcia-305 <garcs336@gmail.com>
2025-11-05 22:34:08 -0600

    Delete Backend directory

--- Backend/src/routes/poke.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getPokemons } from '../controllers/poke.controller.js';
-
-const router = Router();
-
-//Rutas
-router.get('/pokemons', getPokemons);
-
-export default router;
